e221ebf6c3d0d927f147a8a7dbbd08518cfaee85
matt-cassinelli <mdcassinelli@outlook.com>
2022-09-24 03:22:43 +0100

    Implement keyboard controls

--- src/scenes/scene1.ts
+++ src/scenes/scene1.ts
@@ -2,24 +2,26 @@ export class Scene1 extends Phaser.Scene {
 
   private platforms? : Phaser.Physics.Arcade.StaticGroup
   private player? : Phaser.Physics.Arcade.Sprite
+  private cursors? : Phaser.Types.Input.Keyboard.CursorKeys
 
   constructor() {
-    super('Scene1');
+    super('Scene1')
   }
 
   preload() {
     this.load.image('background', 'images/sky.png')
-    this.load.image('ground',     'images/platform.png');
-    this.load.image('star',       'images/star.png');
-    this.load.image('bomb',       'images/bomb.png');
+    this.load.image('ground',     'images/platform.png')
+    this.load.image('star',       'images/star.png')
+    this.load.image('bomb',       'images/bomb.png')
     this.load.spritesheet(
-      'dude', // spritesheets contain frames for animations
+      'dude', // Spritesheets contain frames for animations.
       'images/dude.png',
       {frameWidth: 32, frameHeight: 48}
-    );
+    )
   }
 
   create() {
+
     this.add.image(400, 300, 'background')
     
     this.platforms = this.physics.add.staticGroup();
@@ -30,17 +32,18 @@ export class Scene1 extends Phaser.Scene {
     this.platforms.create(600, 400, 'ground')
     this.platforms.create(750, 220, 'ground')
 
+    //            this.add.sprite // Non-physics version
     this.player = this.physics.add.sprite(100, 450, 'dude')
     this.player.setBounce(0.2)
-    this.player.setCollideWorldBounds(true) // Can't go off screen.
+    this.player.setCollideWorldBounds(true) // The player should not leave the screen.
 
-    this.physics.add.collider(this.player, this.platforms)
+    this.physics.add.collider(this.player, this.platforms) // The player should collide with platforms.
 
     this.anims.create({
       key: 'left',
       frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
       frameRate: 10,
-      repeat: -1 // Infinity (loop forever).
+      repeat: -1 // Loop forever.
     })
 
     this.anims.create({
@@ -55,9 +58,31 @@ export class Scene1 extends Phaser.Scene {
       frameRate: 10,
       repeat: -1
     })
+
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   update() {
+
+    if (!this.cursors) { // Nothing being pressed
+      return
+    }
+
+    if (this.cursors.left?.isDown) { // Optional chaining (sometimes a cursor is undefined)
+      this.player?.setVelocityX(-160)
+      this.player?.anims.play('left', true)
+    }
+    else if (this.cursors.right?.isDown) {
+      this.player?.setVelocityX(160)
+      this.player?.anims.play('right', true)
+    }
+    else {
+      this.player?.setVelocityX(0)
+      this.player?.anims.play('turn', true)
+    }
+
+    if (this.cursors.up?.isDown && this.player?.body.touching.down) {
+      this.player.setVelocityY(-330)
+    }
   }
-  
 }
