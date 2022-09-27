2cbd18274511d6d84d25e02bfb953cd0f85ce012
matt-cassinelli <mdcassinelli@outlook.com>
2022-09-27 21:23:48 +0100

    Move Player into its own class / file

---
+++ src/objects/player.ts
@@ -0,0 +1,72 @@
+import { Physics } from "phaser"
+
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    isDead: Boolean = false;
+    scene: Phaser.Scene
+
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y, 'dude')
+        this.scene = scene;
+        this.initGraphics;
+        this.initPhysics;
+    }
+
+    private initGraphics() {
+      this.scene.add.existing(this); // Add this Player to the scene
+      this.setScale(0.5);
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1 // Loop forever.
+      })
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1
+      })
+      this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4} ],
+        frameRate: 20
+      })
+    }
+  
+    private initPhysics() {
+      this.scene.physics.add.existing(this); // Add this Player's Arcade Physics Body to the scene -required before any physics methods can be used (e.g. setBounce)
+      this.setBounce(0.2);
+      this.setCollideWorldBounds(true); // Prevent leaving the screen.
+      //this.body.setVelocity(100, 200);
+      //this.scene.physics.world.enable(this);
+    }
+
+    update(cursors: any) {
+      if (!cursors) { // Nothing being pressed
+        return
+      }
+      if (cursors.left?.isDown) { // Optional chaining (sometimes a cursor is undefined)
+        this?.setVelocityX(-160)
+        this?.anims.play('left', true)
+      }
+      else if (cursors.right?.isDown) {
+        this?.setVelocityX(160)
+        this?.anims.play('right', true)
+      }
+      else {
+        this?.setVelocityX(0)
+        this?.anims.play('turn', true)
+      }
+      if (cursors.up?.isDown && this?.body.touching.down) {
+        this?.setVelocityY(-330) // Jump.
+      }
+    }
+
+    kill() {
+      //console.log(this); // Debug
+      this.isDead = true;
+      this.scene.cameras.main.shake(500, 0.02)
+      this.setTint(0xff0000)
+      this.anims.play('turn')
+    }
+}
--- src/scenes/scene1.ts
+++ src/scenes/scene1.ts
@@ -1,15 +1,17 @@
 import { Physics } from "phaser"
+import { Player } from '../objects/player'
 
 export class Scene1 extends Phaser.Scene {
 
   private platforms?: Phaser.Physics.Arcade.StaticGroup // "?" means it could be undefined.
   private gold?: Phaser.Physics.Arcade.Group
   private bombs?: Phaser.Physics.Arcade.Group
-  private player?: Phaser.Physics.Arcade.Sprite
+  //private player?: Phaser.Physics.Arcade.Sprite
+  private player?: Player
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
   private score: number = 0
   private scoreText?: Phaser.GameObjects.Text // [todo] Move these to the constructor or create().
-  private gameOver = false
+  //private gameOver = false
 
   constructor() {
     super('Scene1')
@@ -27,45 +29,23 @@ export class Scene1 extends Phaser.Scene {
     )
   }
 
+  createPlatforms() {
+    const platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+    platforms.create(50,  250, 'ground')
+    platforms.create(600, 400, 'ground')
+    platforms.create(750, 220, 'ground')
+    return platforms
+  }
+
   create() {
     this.add.image(400, 300, 'background')
     
-    this.platforms = this.physics.add.staticGroup();
-    // const ground = this.platforms.create(400, 568, 'ground') as Phaser.Physics.Arcade.Image
-    // ground.setScale(2).refreshBody()
-    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody()
-    this.platforms.create(50,  250, 'ground')
-    this.platforms.create(600, 400, 'ground')
-    this.platforms.create(750, 220, 'ground')
+    this.platforms = this.createPlatforms();
 
-    //            this.add.sprite // Non-physics version
-    this.player = this.physics.add.sprite(100, 450, 'dude')
-    this.player.setBounce(0.2)
-    this.player.setCollideWorldBounds(true) // The player should not leave the screen.
+    this.player = new Player(this, 100, 450)
 
     this.physics.add.collider(this.player, this.platforms) // The player should collide with platforms.
-
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-      frameRate: 10,
-      repeat: -1 // Loop forever.
-    })
-
-    this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'dude', frame: 4} ],
-      frameRate: 20
-    })
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-      frameRate: 10,
-      repeat: -1
-    })
-
-    this.cursors = this.input.keyboard.createCursorKeys()
 
     this.gold = this.physics.add.group({
       key: 'gold',
@@ -86,7 +66,9 @@ export class Scene1 extends Phaser.Scene {
 
     this.bombs = this.physics.add.group()
     this.physics.add.collider(this.bombs, this.platforms)
-    this.physics.add.collider(this.player, this.bombs, this.handleHitBomb, undefined, this)
+    this.physics.add.collider(this.player, this.bombs, this.player.kill, undefined, this.player)
+
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   private handleCollectGold(player: Phaser.GameObjects.GameObject, gold: Phaser.GameObjects.GameObject) {
@@ -115,33 +97,15 @@ export class Scene1 extends Phaser.Scene {
     }
   }
 
-  private handleHitBomb(player: Phaser.GameObjects.GameObject, bomb: Phaser.GameObjects.GameObject) {
-    this.physics.pause() // this should be a kill method inside Player
-    this.player?.setTint(0xff0000)
-    this.player?.anims.play('turn')
-    this.gameOver = true
-  }
-
   update() {
-    if (!this.cursors) { // Nothing being pressed
-      return
-    }
-
-    if (this.cursors.left?.isDown) { // Optional chaining (sometimes a cursor is undefined)
-      this.player?.setVelocityX(-160)
-      this.player?.anims.play('left', true)
-    }
-    else if (this.cursors.right?.isDown) {
-      this.player?.setVelocityX(160)
-      this.player?.anims.play('right', true)
-    }
-    else {
-      this.player?.setVelocityX(0)
-      this.player?.anims.play('turn', true)
-    }
-
-    if (this.cursors.up?.isDown && this.player?.body.touching.down) {
-      this.player.setVelocityY(-330) // Jump.
+    this.player?.update(this.cursors)
+    if (this.player?.isDead) {
+      this.physics.pause()
+      this.time.addEvent({
+        delay: 1500,
+        callback: () => this.scene.restart()
+      })
+      //this.gameOver = true // What's the use in this?
     }
   }
 }
